e3f1cd0cf1004598c8b17528d55149344499125c
ritsu22 <my.pace.r.n@gmail.com>
2021-07-16 17:17:09 +0900

    Composite component

--- src/components/Task.js
+++ src/components/Task.js
@@ -6,21 +6,6 @@ export default function Task({
   onArchiveTask,
   onPinTask,
 }) {
-  Task.propTypes = {
-    /** Composition of the task */
-    task: PropTypes.shape({
-      /** Id of the task */
-      id: PropTypes.string.isRequired,
-      /** Title of the task */
-      title: PropTypes.string.isRequired,
-      /** Current state of the task */
-      state: PropTypes.string.isRequired,
-    }),
-    /** Event to change the task to archived */
-    onArchiveTask: PropTypes.func,
-    /** Event to change the task to pinned */
-    onPinTask: PropTypes.func,
-  };
   return (
     <div className={`list-item ${state}`}>
       <label className="checkbox">
@@ -52,3 +37,19 @@ export default function Task({
     </div>
   );
 }
+
+Task.propTypes = {
+  /** Composition of the task */
+  task: PropTypes.shape({
+    /** Id of the task */
+    id: PropTypes.string.isRequired,
+    /** Title of the task */
+    title: PropTypes.string.isRequired,
+    /** Current state of the task */
+    state: PropTypes.string.isRequired,
+  }),
+  /** Event to change the task to archived */
+  onArchiveTask: PropTypes.func,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+};
